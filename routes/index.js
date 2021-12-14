ca5548ade8b13fa5b97ce818abc5b2e08887930d
BuildTools <sarquentin@gmail.com>
2021-12-14 23:54:03 +0100

    Adding middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require("express");
 const { All } = require("../api/controllers/CarteController");
 const { Update } = require("../api/controllers/UpdateController");
+const AppToken = require("../api/middlewares/AppToken");
 var router = express.Router();
 
-router.get("/cartes", (req, res) => All(req, res));
-router.post("/update", (req, res) => Update(req, res));
+router.get(
+  "/cartes",
+  (req, res, next) => AppToken(req, res, next),
+  (req, res) => All(req, res)
+);
+router.post(
+  "/update",
+  (req, res, next) => AppToken(req, res, next),
+  (req, res) => Update(req, res)
+);
 
 module.exports = router;
